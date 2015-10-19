89da6a1c263bde1a7ed7dcc818e4c44cb1e57c03
Mathias R. Larsen <mathiaslarsen@10.16.161.195>
2015-10-19 14:18:00 +0200

    Første commit

---
+++ Test/src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+public static void main(String[] args) {
+	
+	int x = 3; 
+	int counter = 0;
+	
+	while ((x-1) <0) {
+		++counter;
+		x--;
+		System.out.println("Counter er: " + counter);
+	}
+	
+}
+}
